73185e5d5492c56a48d880a3c65fa22d6f9655c8
suraj <surajrajapandian@gmail.com>
2022-09-22 19:18:16 +0530

    Second

--- science/settings.py
+++ science/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ka_!uq=z$)%!0c_vl+pj)v@uk)k=$jj@#=7kvest)kjb)d44e@'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
